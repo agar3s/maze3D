40db0d8de8c3e26a2d710fee2b4b809b17ffdf64
agares <ekeisco@gmail.com>
2013-07-22 04:02:23 -0500

    adds some gaming =)

--- walls.js
+++ walls.js
@@ -1,22 +1,104 @@
-var walls = [{x:1, z:2, orientation:'back'},
+var walls = [
+{x:1, z:2, orientation:'front'},
+{x:1, z:2, orientation:'right'},
+{x:1, z:2, orientation:'back'},
 {x:1, z:7, orientation:'back'},
 {x:1, z:7, orientation:'right'},
 {x:1, z:9, orientation:'front'},
 {x:1, z:9, orientation:'right'},
 
-{x:2, z:6, orientation:'front'},
-{x:2, z:6, orientation:'back'},
+{x:2, z:5, orientation:'front'},
+{x:2, z:5, orientation:'back'},
+{x:2, z:5, orientation:'left'},
+{x:2, z:7, orientation:'front'},
+{x:2, z:7, orientation:'back'},
+{x:2, z:7, orientation:'left'},
+{x:2, z:7, orientation:'right'},
 
 {x:3, z:1, orientation:'right'},
+{x:3, z:1, orientation:'left'},
 {x:3, z:2, orientation:'right'},
+{x:3, z:2, orientation:'left'},
 {x:3, z:2, orientation:'back'},
-{x:3, z:3, orientation:'right'},
 {x:3, z:5, orientation:'back'},
 {x:3, z:5, orientation:'right'},
+{x:3, z:5, orientation:'front'},
 {x:3, z:6, orientation:'right'},
 
+{x:4, z:3, orientation:'front'},
+{x:4, z:3, orientation:'back'},
+{x:4, z:3, orientation:'left'},
+{x:4, z:3, orientation:'right'},
+{x:4, z:5, orientation:'front'},
 {x:4, z:7, orientation:'front'},
 {x:4, z:7, orientation:'right'},
 {x:4, z:9, orientation:'right'},
 
-{x:6, z:4, orientation:'right'}];
+
+{x:5, z:4, orientation:'right'},
+{x:5, z:4, orientation:'back'},
+{x:5, z:7, orientation:'back'},
+{x:5, z:9, orientation:'right'},
+{x:5, z:9, orientation:'front'},
+
+
+{x:6, z:3, orientation:'front'},
+{x:6, z:3, orientation:'left'},
+{x:6, z:7, orientation:'back'},
+
+{x:7, z:2, orientation:'front'},
+{x:7, z:2, orientation:'left'},
+{x:7, z:8, orientation:'back'},
+{x:7, z:8, orientation:'front'},
+{x:7, z:8, orientation:'left'},
+{x:7, z:8, orientation:'right'},
+
+{x:8, z:2, orientation:'front'},
+{x:8, z:2, orientation:'right'},
+{x:8, z:3, orientation:'right'},
+{x:8, z:4, orientation:'right'},
+{x:8, z:5, orientation:'right'},
+{x:8, z:6, orientation:'right'},
+{x:8, z:7, orientation:'right'},
+{x:8, z:8, orientation:'right'},
+{x:8, z:9, orientation:'right'},
+
+{x:9, z:1, orientation:'back'},
+{x:9, z:7, orientation:'back'},
+
+{x:10, z:1, orientation:'back'},
+{x:10, z:5, orientation:'back'},
+{x:10, z:5, orientation:'front'},
+{x:10, z:5, orientation:'left'},
+{x:10, z:7, orientation:'back'},
+
+{x:11, z:1, orientation:'back'},
+{x:11, z:5, orientation:'back'},
+{x:11, z:5, orientation:'front'},
+{x:11, z:7, orientation:'back'},
+
+{x:12, z:1, orientation:'back'},
+{x:12, z:5, orientation:'back'},
+{x:12, z:5, orientation:'front'},
+{x:12, z:7, orientation:'back'},
+
+{x:13, z:1, orientation:'back'},
+{x:13, z:5, orientation:'back'},
+{x:13, z:5, orientation:'front'},
+{x:13, z:5, orientation:'right'},
+{x:13, z:7, orientation:'back'},
+
+{x:14, z:2, orientation:'back'},
+{x:14, z:2, orientation:'front'},
+{x:14, z:2, orientation:'left'},
+{x:14, z:2, orientation:'right'},
+{x:14, z:4, orientation:'front'},
+{x:14, z:4, orientation:'left'},
+{x:14, z:8, orientation:'back'},
+{x:14, z:8, orientation:'front'},
+{x:14, z:8, orientation:'left'},
+{x:14, z:8, orientation:'right'},
+
+{x:15, z:4, orientation:'front'}
+
+];
